60da355e47d3491a7a226e5c9081599f4cd07032
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2026-06-23 07:51:56 +0545

    add RegisterRoutes

---
+++ internal/routes/routes.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"go_crud_postgres/internal/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(router *gin.Engine, userHandler *user.UserHandler) {
+	api := router.Group("/api")
+	v1 := api.Group("/v1")
+
+	users := v1.Group("/users")
+	{
+		// users.GET("", userHandler.GetUsers)
+		// users.GET("/:id", userHandler.GetUserById)
+		users.POST("", userHandler.CreateUser)
+		// users.PUT("/:id", userHandler.UpdateUser)
+		// users.PATCH("/:id", userHandler.PatchUser)
+		// users.DELETE("/:id", userHandler.DeleteUser)
+	}
+}
