7f85a0ec75c48b2bbe514adea6317357a7a1ccff
Kshitiz Adhikari <adhikarikshitiz12@gmail.com>
2026-06-23 07:52:44 +0545

    add CreateUser handler, service

---
+++ internal/user/handler.go
@@ -0,0 +1,43 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	service *UserService
+}
+
+func NewUserHandler(service *UserService) *UserHandler {
+	return &UserHandler{
+		service: service,
+	}
+}
+
+func (h *UserHandler) CreateUser(c *gin.Context) {
+	var req CreateUserRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := h.service.CreateUser(c.Request.Context(), req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "user created successfully",
+		"data":    user.ToResponse(),
+	})
+
+}
+
+// func (h *UserHandler) GetAll() {
+// 	users := h.service.GetAll()
+
+// }
---
+++ internal/user/service.go
@@ -0,0 +1,48 @@
+package user
+
+import (
+	"context"
+	"fmt"
+
+	"go_crud_postgres/internal/models"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService struct {
+	repo *UserRepository
+}
+
+func NewUserService(repo *UserRepository) *UserService {
+	return &UserService{repo: repo}
+}
+
+type CreateUserRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+func (s *UserService) CreateUser(ctx context.Context, req CreateUserRequest) (*models.User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	// Create user
+	user := &models.User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Password: string(hashedPassword),
+	}
+
+	if err := s.repo.Create(ctx, user); err != nil {
+		return nil, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return user, nil
+}
+
+// func (s *UserService) GetAll() *[]User {
+
+// }
